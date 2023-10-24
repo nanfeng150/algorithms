d9f8714c45208f211cd2f6df7ef194357e06e02f
nanfeng150 <3403249524@qq.com>
2023-10-24 20:11:53 +0800

    实现高精度加法

--- 高精度加法（大整数加法）/高精度加法（大整数加法）/源.cpp
+++ 高精度加法（大整数加法）/高精度加法（大整数加法）/源.cpp
@@ -2,7 +2,34 @@
 #include<iostream>
 #include<string>
 using namespace std;
+string func(string str1, string str2) {
+	string result = "";
+	int str1Len = str1.length();
+	int str2Len = str2.length();
+	int ptr1 = str1Len - 1, ptr2 = str2Len - 1;
+	bool flag = false;//标记是否有进位
+	while (ptr1 >= 0 || ptr2 >= 0) {
+		int left = ptr1 < 0 ? 0 : str1[ptr1] - '0';
+		int right = ptr2 < 0 ? 0 : str2[ptr2] - '0';
+		int temp = left + right + flag;
+		if (temp < 10) {
+            result.insert(result.begin(), temp + '0');
+			flag = false;
+		}	
+		else {
+			result.insert(result.begin(), temp % 10 + '0');
+			flag = true;
+		}
+		ptr1--;
+		ptr2--;
+	}
+	if(flag)
+		result.insert(result.begin(), 1 + '0');
+	return result;
+}
 int main() {
-
+	string str1, str2;
+	cin >> str1 >> str2;
+	cout << "result = " << func(str1, str2) << endl;
 	return 0;
 }
